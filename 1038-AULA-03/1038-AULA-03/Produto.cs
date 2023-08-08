7c42ed58284c050ed9199dd61454a7392d35fd2a
Karen Viana <karenpjvianaa@gmail.com>
2023-08-08 18:45:18 -0300

    Commit inicial questão aula 3

---
+++ 1038-AULA-03/1038-AULA-03/Produto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1038_AULA_03
+{
+    internal class Produto
+    {
+        public string nome { get; set; }
+        public double preco { get; set; }
+        public int quantidade { get; set; }
+        public double valorTotal { get; set; }
+
+        public double ValorTotalEstoque()
+        {
+            return valorTotal = preco * quantidade;
+        }
+
+        public void AdicionarProdutos(int qtde) 
+        {
+            Console.WriteLine($"====== Estoque ======{Environment.NewLine}" +
+                              $"Nome: {Environment.NewLine}{nome}" + 
+                              $"Preço: {Environment.NewLine}{preco}" +
+                              $"Quantidade: {Environment.NewLine}{quantidade + qtde}" +
+                              $"Valor Total no estoque: {Environment.NewLine}{valorTotal}");
+        }
+
+        public void RemoverProdutos(int qtde)
+        {
+            Console.WriteLine($"====== Estoque ======{Environment.NewLine}" +
+                              $"Nome: {Environment.NewLine}{nome}" +
+                              $"Preço: {Environment.NewLine}{preco}" +
+                              $"Quantidade: {Environment.NewLine}{quantidade - qtde}" +
+                              $"Valor Total no estoque: {Environment.NewLine}{valorTotal}");
+        }
+    }
+}
---
+++ 1038-AULA-03/1038-AULA-03/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _1038_AULA_03
+{
+    internal class Program
+    { 
+        enum Opcao { Adicionar = 1, Remover}
+        static void Main(string[] args)
+        {
+            /* Fazer um programa para ler os dados de um produto em estoque(nome, preço e quantidade no estoque). 
+                * Em seguida:
+                * • Mostrar os dados do produto(nome, preço, quantidade no estoque, valor total no estoque)
+                * • Realizar uma entrada no estoque e mostrar novamente os dados do produto
+                * • Realizar uma saída no estoque e mostrar novamente os dados do produto.*/
+
+            Produto produto = new();
+            Console.WriteLine("====== Estoque ======");
+            Console.WriteLine("Informe o nome do produto:");
+            produto.nome = Console.ReadLine();
+            Console.WriteLine("Informe o preço do produto:");
+            produto.preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o quantidade do produto no estoque:");
+            produto.quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"====== Estoque ======{Environment.NewLine}" +
+                              $"Nome: {Environment.NewLine}{produto.nome}" +
+                              $"Preço: {Environment.NewLine}{produto.preco}" +
+                              $"Quantidade: {Environment.NewLine}{produto.quantidade}" +
+                              $"Valor Total no estoque: {Environment.NewLine}{produto.valorTotal}");
+
+            Console.WriteLine("Informe o quantidade do produto no estoque:");
+            int opc = int.Parse(Console.ReadLine());
+            Opcao opcao = (Opcao)opc;
+            switch (opcao) {
+                case Opcao.Adicionar:
+                    Produto.AdicionarProdutos();
+
+            }
+
+        }
+    }
+}
